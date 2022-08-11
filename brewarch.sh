b48b46cc9170b892bc91879e637a4dccf4e8e878
Ansh <ANSHSINGH25BD@GMAIL.COM>
2022-08-11 15:53:02 +0530

    better implementation

--- brewarch.sh
+++ brewarch.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+user=$(whoami)
 sudo pacman -S base-devel
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-[ -d /home/linuxbrew/.linuxbrew ] && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo '$(eval) "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/$user/.bash_profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew doctor
 brew config
 while getopts ":e" option; do
